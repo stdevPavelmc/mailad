fbc3e58056932ce22d2396be7c5e77d6d71869fd
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-11-15 10:33:00 -0500

    Added the new test script for localhost

---
+++ scripts/test_localhost.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# This script is part of MailAD, see https://github.com/stdevPavelmc/mailad/
+#
+# Goals:
+#   - Test the local configuration for:
+#       - fqdn & domain
+#       - DNS resolution and HOSTAD config
+#       - conectivity to the HOSTAD 
+
+# locate the source file (makefile or run by hand)
+if [ -f mailad.conf ] ; then 
+    source mailad.conf
+else
+    source ../mailad.conf
+fi
+
+echo "Testing the configurations on the local host"
+
+# hostname vs fqdn
+HOST=`hostname`
+FQDN=`hostname -f`
+if [ "$HOST" == "$FQDN" ] ; then
+    # fail
+    echo "ERROR!"
+    echo "    Your hostname does not have a domain or it's configured wrong!"
+    echo "    A 'hostname' command must return just the name of the host with no domain"
+    echo "    A 'hostname -f' command must return the name with the domain"
+    exit 1
+else
+    echo "You have a correct fqdn in the hostname"
+    echo "Success!"
+fi
+
+# localhost is localhost?
+if [ "$HOSTNAME" == "$FQDN" ] ; then
+    echo "You have a correct HOSTNAME configured"
+    echo "Success!"
+else
+     # fail
+    echo "ERROR!"
+    echo "    Your HOSTNAME var in mailad.conf does not match the FQDN of this host!"
+    echo "    Please fix that"
+    exit 1
+fi
+
+# ping the hostad
+ping -c 3 "$HOSTAD"
+R=$?
+if [ $R -eq 0 ] ; then
+    echo "The domain host listed in HOSTAD is network reacheable!"
+    echo "Success!"
+else
+    # fail
+    echo "ERROR!"
+    echo "    Domain host seems to be down or not reacheable!"
+    echo "    Check your network settings and cable"
+    exit 1
+fi
+
+# check if the DNS is working, by asking for the SOA of the domain
+SOAREC=`dig SOA $DOMAIN +short`
+if [ "$SOAREC" == "" ] ; then
+    # fail
+    echo "ERROR!"
+    echo "    The DOMAIN you declared in mailad.conf has no SOA record in the actual DNS"
+    echo "    That, or your DNS is not configurated correctly in this host"
+    exit 1
+else
+    # returned values so DNS is configured OK, but need more testing
+    echo "SOA record acquired, testing that HOSTAD points to the SOA..."
+    echo "Success!"
+
+    HOST=`echo $SOAREC | grep $HOSTAD`
+    if [ "$HOST" == "" ] ; then
+        # maybe HOSTAD is an IP?
+        HOST=`echo $SOAREC | awk '{print $1}' | rev | cut -d "." -f 2- | rev`
+        IP=`dig A $HOST +short`
+        if [ "IP" == "$HOSTAD" ] ; then
+             # success
+            echo "The SOA record points to the HOSTAD value (HOSTAD is an IP), nice!"
+            echo "Success!"
+        else
+            # fail
+            echo "ERROR!"
+            echo "    The DNS answer with a value for the SOA of $DOMAIN, but the value does"
+            echo "    not match the one configured in HOSTAD, please fix that"
+            echo "    HOSTAD=$HOSTAD vs SOA_IP=$IP "
+            exit 1
+        fi
+    else
+        # success
+        echo "The SOA record points to the HOSTAD value (HOSTAD is a hostname), nice!"
+        echo "Success!"
+    fi
+fi
