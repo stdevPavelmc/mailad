b5161fae97c0189202f67ef2fcc349dcb64c3d77
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2020-07-25 02:19:56 -0400

    Improved the log dump & the grammar, added the NATIONAL & LOCAL test for the new feature

--- tests/test.sh
+++ tests/test.sh
@@ -44,6 +44,7 @@ fi
 
 # others
 LOG=./test.log
+LOGP=./latest.log
 cat /dev/null > $LOG
 
 # Reset the locales
@@ -60,39 +61,41 @@ echo "Using server: $SERVER"
 echo " "
 
 ### Send an email to the mail admin: port 25
-$SOFT -s $SERVER --protocol SMTP -t $ADMINMAIL >> $LOG
+$SOFT -s $SERVER --protocol SMTP -t $ADMINMAIL > $LOGP
 R=$?
 if [ $R -ne 0 ] ; then
     # error
     echo "======================================================"
-    echo "ERROR: can't send a mail to a valid local email via"
-    echo "       using SMTP (25)"
+    echo "ERROR: Can't send a mail to a valid local email using"
+    echo "       simple SMTP (25)"
     echo " "
-    echo "COMMENT: it's expected that your server can receive"
+    echo "COMMENT: It's expected that your server can receive"
     echo "         emails for it's domain, please check your"
     echo "         configuration"
     echo " "
     echo "Exit code: $R"
     echo "Logs follow"
     echo "======================================================"
-    tail -n 20 $LOG
+    cat $LOGP
     exit 1
 else
     # ok
     echo "===> Ok: You can receive emails for your domain"
 fi
+# sum the logs
+cat $LOGP > $LOG
 
 ### Send an email to the mail admin with auth as sender
 . $PATHPREF/.mailadmin.auth
-$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$ADMINMAIL" -ap "$PASS" -t "$ADMINMAIL" -f "$ADMINMAIL"  >> $LOG
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$ADMINMAIL" -ap "$PASS" -t "$ADMINMAIL" -f "$ADMINMAIL"  > $LOGP
 R=$?
 if [ $R -ne 0 ] ; then
     # error
     echo "========================================================="
-    echo "ERROR: can't relay a mail to a valid local recipient with"
+    echo "ERROR: Can't relay a mail to a valid local recipient with"
     echo "       authentication (as sender) over SUBMISSION (587)"
     echo " "
-    echo "COMMENT: it's expected that your server can receive"
+    echo "COMMENT: It's expected that your server can receive"
     echo "         emails for it's domain via SUBMISSION from an"
     echo "         authenticated user, please check your"
     echo "         configuration"
@@ -100,161 +103,326 @@ if [ $R -ne 0 ] ; then
     echo "Exit code: $R"
     echo "Logs follow"
     echo "========================================================="
-    tail -n 20 $LOG
+    cat $LOGP
     exit 1
 else
     # ok
     echo "===> Ok: Authenticated users can send local emails"
 fi
+# sum the logs
+cat $LOGP >> $LOG
 
 ### Send an email to the outside as a valid user with auth
-$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$ADMINMAIL" -ap "$PASS" -t "fake@example.com" -f "$ADMINMAIL"  >> $LOG
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$ADMINMAIL" -ap "$PASS" -t "fake@example.com" -f "$ADMINMAIL"  > $LOGP
 R=$?
 if [ $R -ne 0 ] ; then
     # error
     echo "=========================================================="
-    echo "ERROR: can't send a mail to an outside recipient using"
+    echo "ERROR: Can't send a mail to an outside recipient using"
     echo "       authentication (as sender) from a local user over"
     echo "       SUBMISSION (587)"
     echo " "
-    echo "COMMENT: it's expected that your server can send an"
-    echo "         email to the outside world via SUBMISSION from"
-    echo "         an authenticated local user, please check your"
+    echo "COMMENT: It's expected that your server can send an email"
+    echo "         to the outside world via SUBMISSION from an"
+    echo "         authenticated local user, please check your"
     echo "         configuration"
     echo " "
     echo "Exit code: $R"
     echo "Logs follow"
     echo "=========================================================="
-    tail -n 20 $LOG
+    cat $LOGP
     exit 1
 else
     # ok
     echo "===> Ok: Authenticated users can send emails to the outside world"
 fi
+# sum the logs
+cat $LOGP >> $LOG
 
 ### Send an email to a non-existent user, port 25
 USER=`mktemp | cut -d '/' -f 3`
-$SOFT -s $SERVER --protocol SMTP -t "$USER@$DOMAIN" >> $LOG
+$SOFT -s $SERVER --protocol SMTP -t "$USER@$DOMAIN" > $LOGP
 R=$?
 if [ $R -ne 24 ] ; then
     # error
     echo "=========================================================="
-    echo "ERROR: can send a mail to a non-valid local email via SMTP"
+    echo "ERROR: Can send a mail to a non-valid local email via SMTP"
     echo " "
-    echo "COMMENT: it's expected that your server bounce mails for"
+    echo "COMMENT: It's expected that your server bounce mails for"
     echo "         unknown recipients of your domain, please check"
     echo "         your configuration"
     echo " "
     echo "Exit code: $R"
     echo "Logs follow"
     echo "=========================================================="
-    tail -n 20 $LOG
+    cat $LOGP
     exit 1
 else
     # ok
     echo "===> Ok: Your server reject unknown recipients"
 fi
+# sum the logs
+cat $LOGP >> $LOG
 
 ### Send an email to an external user as an outsider domain, port 25 (open relay)
 USER=`mktemp | cut -d '/' -f 3`
 USER1=`mktemp | cut -d '/' -f 3`
-$SOFT -s $SERVER --protocol SMTP -t "$USER@example" -f "$USER1@example" >> $LOG
+$SOFT -s $SERVER --protocol SMTP -t "$USER@example" -f "$USER1@example" > $LOGP
 R=$?
 if [ $R -ne 24 ] ; then
     # error
     echo "========================================================"
-    echo "ERROR: can relay a mail not for your domain and comming"
+    echo "ERROR: Can relay a mail not for your domain and comming"
     echo "       from a external domain"
     echo " "
-    echo "COMMENT: it's expected that your server does not relay"
+    echo "COMMENT: It's expected that your server does not relay"
     echo "         mails for other domains than the configured one"
     echo "         so you are an OPEN REALY server, and that is"
-    echo "         bad, very bad, please check your configuration"
+    echo "         bad, VERY BAD, please check your configuration"
     echo " "
     echo "Exit code: $R"
     echo "Logs follow"
     echo "========================================================"
-    tail -n 20 $LOG
+    cat $LOGP
     exit 1
 else
     # ok
     echo "===> Ok: Your server is not and open relay"
 fi
+# sum the logs
+cat $LOGP >> $LOG
 
 ### Send an email to the mail admin: port SSMTP
-$SOFT -s "$SERVER" --protocol SSMTP -t $ADMINMAIL >> $LOG
+$SOFT -s "$SERVER" --protocol SSMTP -t $ADMINMAIL > $LOGP
 R=$?
 if [ $R -ne 24 ] ; then
     # error
     echo "======================================================="
-    echo "ERROR: can send a mail to a valid local email via SSMTP"
+    echo "ERROR: Can send a mail to a valid local email via SSMTP"
     echo "       with no authentication"
     echo " "
-    echo "COMMENT: it's expected that your server bounce emails"
-    echo "         via secure channel with no authentication;"
-    echo "         by doing this mails for other domains, if so you are an OPEN"
-    echo "         RELAY and that is bad, very bad, please check your"
-    echo "         configuration"
-    echo " "
-    echo "Exit code: $R"
-    echo "Logs follow"
-    echo "======================================================"
-    tail -n 20 $LOG
-    exit 1
-else
-    # ok
-    echo "===> Ok: The server rejects relaying mail though unauthenticated SMTPS"
-fi
-
-### Send an email as an user and auth as other (id spoofing)
-$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$ADMINMAIL" -ap "$PASS" -t "$ADMINMAIL" -f "$USER@$DOMAIN" >> $LOG
-R=$?
-if [ $R -ne 24 ] ; then
-    # error
-    echo "========================================================="
-    echo "ERROR: can send a mail to a valid local email using"
-    echo "       SUBMISSION and a valid authentication that does"
-    echo "       not match the sender address, please check your"
-    echo "       configuration"
-    echo " "
-    echo "COMMENT: it's expected that your server bounce emails"
-    echo "         from a sender that authenticated as another user"
-    echo "         this is a id spoofing technique, please check"
-    echo "         your configuration"
-    echo " "
-    echo "Exit code: $R"
-    echo "Logs follow"
-    echo "========================================================="
-    tail -n 20 $LOG
-    exit 1
-else
-    # ok
-    echo "===> Ok: The server does NOT allow id spoofing"
-fi
-
-### Send an email to the mail admin with an attachment bigger than the allowed: port 25
-MS=`echo "$MESSAGESIZE*1024*1024*1.2" | bc -q | cut -d '.' -f 1`
-TMP=`mktemp`
-dd if=/dev/zero of=$TMP bs=1 count="$MS" 2>/dev/null
-$SOFT -s $SERVER --protocol SMTP -t $ADMINMAIL --attach "$TMP" >> $LOG
-rm $TMP
-R=$?
-if [ $R -ne 0 ] ; then
-    # error
-    echo "======================================================"
-    echo "ERROR: can send a mail to a valid local email via SMTP"
-    echo "    with an attachement bigger than the allowed"
-    echo " "
-    echo "COMMENT: it's expected that your server bounce emails"
-    echo "         that are bigger that the maximium allowed,"
+    echo "COMMENT: It's expected that your server bounce emails"
+    echo "         via secure channel with no authentication,"
     echo "         please check your configuration"
     echo " "
     echo "Exit code: $R"
     echo "Logs follow"
     echo "======================================================"
-    tail -n 20 $LOG
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: The server rejects relaying mail though unauthenticated SMTPS"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### Send an email as an user and auth as other (id spoofing)
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$ADMINMAIL" -ap "$PASS" -t "$ADMINMAIL" -f "$USER@$DOMAIN" > $LOGP
+R=$?
+if [ $R -ne 24 ] ; then
+    # error
+    echo "========================================================="
+    echo "ERROR: Can send a mail to a valid local email using"
+    echo "       SUBMISSION and a valid authentication that does"
+    echo "       not match the sender address"
+    echo " "
+    echo "COMMENT: It's expected that your server bounce emails"
+    echo "         from a sender that authenticated as another user"
+    echo "         this is a well known id spoofing technique,"
+    echo "         please check your configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "========================================================="
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: The server does NOT allow id spoofing"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### Send an email to the mail admin with an attachment bigger than the allowed: port 25
+MS=`echo "$MESSAGESIZE*1024*1024*1.2" | bc -q | cut -d '.' -f 1`
+TMP=`mktemp`
+dd if=/dev/zero of=$TMP bs=1 count="$MS" 2>/dev/null
+$SOFT -s $SERVER --protocol SMTP -t $ADMINMAIL --attach "$TMP" > $LOGP
+rm $TMP
+R=$?
+if [ $R -ne 0 ] ; then
+    # error
+    echo "======================================================"
+    echo "ERROR: Can send a mail to a valid local email via SMTP"
+    echo "       with an attachement bigger than the stated"
+    echo " "
+    echo "COMMENT: It's expected that your server bounce emails"
+    echo "         that are bigger that the maximium stated,"
+    echo "         please check your configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "======================================================"
+    cat $LOGP
     exit 1
 else
     # ok
     echo "===> Ok: Mail size restriction is working"
 fi
+# sum the logs
+cat $LOGP >> $LOG
+
+# NATIONAL
+
+### Send an email to a national User from an international account: port 25
+$SOFT -s $SERVER --protocol SMTP -t $NACUSER -f "testing@example.com" > $LOGP
+R=$?
+if [ $R -ne 24 ] ; then
+    # error
+    echo "======================================================"
+    echo "ERROR: Can send a mail to a defined national account"
+    echo "       from an international address: using SMTP (25)"
+    echo " "
+    echo "COMMENT: It's expected that your server block this"
+    echo "         emails, as a policy, please check your"
+    echo "         configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "======================================================"
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: National restricted users can't receive emails from outside"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### Send an email to the outside as a national user user with auth
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$NACUSER" -ap "$NACUSERPASSWD" -t "fake@example.com" -f "$NACUSER" > $LOGP
+R=$?
+if [ $R -ne 24 ] ; then
+    # error
+    echo "=========================================================="
+    echo "ERROR: Can send a mail to an internet recipient from a"
+    echo "       national limited account, using SUBMISSION (587)"
+    echo " "
+    echo "COMMENT: It's expected that your server block this as"
+    echo "         the users is limited to national access only,"
+    echo "         please check your configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "=========================================================="
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: National restricted users can't send emails to internet"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### Send an email to a national recipient as a national user user with auth
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$NACUSER" -ap "$NACUSERPASSWD" -t "reject@nonexistent.cu" -f "$NACUSER" > $LOGP
+R=$?
+if [ $R -ne 0 ] ; then
+    # error
+    echo "=========================================================="
+    echo "ERROR: Can't send a mail to a national recipient from a"
+    echo "       national limited account, using SUBMISSION (587)"
+    echo " "
+    echo "COMMENT: It's not the expected, your server must allow the"
+    echo "         user to send the mail, please check your"
+    echo "         configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "=========================================================="
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: National restricted users can send emails to national address"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### LOCAL
+
+### Send an email to a local user from an international account: port 25
+$SOFT -s $SERVER --protocol SMTP -t $LOCUSER -f "testing@example.com" > $LOGP
+R=$?
+if [ $R -ne 24 ] ; then
+    # error
+    echo "======================================================"
+    echo "ERROR: Can send a mail to a defined local account from"
+    echo "       an international address: using SMTP (25)"
+    echo " "
+    echo "COMMENT: It's expected that your server block this"
+    echo "         emails as this users has only local domain"
+    echo "         access, please check your configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "======================================================"
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: Local restricted users can't receive emails from outside"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### Send an email to the outside as a local user user with auth
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$LOCUSER" -ap "$LOCUSERPASSWORD" -t "fake@example.com" -f "$LOCUSER" > $LOGP
+R=$?
+if [ $R -ne 24 ] ; then
+    # error
+    echo "=========================================================="
+    echo "ERROR: Can send a mail to an internet recipient from a"
+    echo "       local limited account, using SUBMISSION (587)"
+    echo " "
+    echo "COMMENT: It's expected that your server block this as"
+    echo "         the users is limited to local access only,"
+    echo "         please check your configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "=========================================================="
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: Local restricted users can't send emails to internet"
+fi
+# sum the logs
+cat $LOGP >> $LOG
+
+### Send an email to a local recipient as a local user user with auth
+$SOFT -s "$SERVER" -p 587 -tls -a PLAIN -au "$LOCUSER" -ap "$LOCUSERPASSWORD" -t "$NACUSER" -f "$LOCUSER" > $LOGP
+R=$?
+if [ $R -ne 0 ] ; then
+    # error
+    echo "=========================================================="
+    echo "ERROR: Can't send a mail to an local recipient from a"
+    echo "       local limited account, using SUBMISSION (587)"
+    echo " "
+    echo "COMMENT: It's not the expected, your server must allow the"
+    echo "         user to send the mail, please check your"
+    echo "         configuration"
+    echo " "
+    echo "Exit code: $R"
+    echo "Logs follow"
+    echo "=========================================================="
+    cat $LOGP
+    exit 1
+else
+    # ok
+    echo "===> Ok: Local restricted users can send emails to local recipients"
+fi
+# sum the logs
+cat $LOGP >> $LOG
