939104074a3e92f0322af463f0c38f000bf8d213
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2020-07-29 20:51:21 -0400

    Switch to /etc/mailad/ for config, this script creates the place

---
+++ scripts/conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is part of MailAD, see https://github.com/stdevPavelmc/mailad/
+# Copyright 2020 Pavel Milanes Costa <pavelmc@gmail.com>
+# LICENCE: GPL 3.0 and later  
+#
+# Goal:
+#   - Create the /etc/mailad folder and copy a default conf to it
+#
+
+# Create the dir with no warning
+mkdir -p /etc/mailad/ &2> /dev/null
+
+# check if there is a conf there
+if [ -f /etc/mailad/mailad.conf ] ; then
+    echo "There is a mailad.conf file already in place, move or erase it"
+    echo "and run 'make conf' one more time to set a default one"
+else
+    # just compy the default
+    echo "Installing default conf file en /etc/cmailad/"
+    cp mailad.conf /etc/mailad/
+fi
