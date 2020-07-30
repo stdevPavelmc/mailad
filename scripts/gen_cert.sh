ef4a37599f85c7575ad35161046f968155b426ec
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2020-07-30 19:26:24 -0400

    First aproximation, need testing...

--- scripts/gen_cert.sh
+++ scripts/gen_cert.sh
@@ -18,39 +18,52 @@ if [ -f /etc/ssl/private/mail.key -a -f /etc/ssl/certs/mail.crt -a -f /etc/ssl/c
     exit 0
 fi
 
-# verify a LE cert
-# openssl verify -verbose -x509_strict -CAfile le/fullchain.pem le/fullchain.pem 
+# Check if a LE certificate is on the config
+if [ -f /etc/mailad/le/fullchain.pem -a -f /etc/mailad/le/privkey.key ] ; then
+    echo "Let's Encrypt certificates found, using them"
 
-echo "Generating a Self Signed Certificate for this node" 
+    # erase in place certificates if found
+    rm -f /etc/ssl/private/mail.key &2> /dev/null
+    rm -f /etc/ssl/certs/mail.crt &2> /dev/null
+    rm -f /etc/ssl/certs/cacert.pem &2> /dev/null
 
-# moving to a temp dir to work 
-TMP=`mktemp -d`
-cd "$TMP"
+    # linking the LE certificates
+    ln -s /etc/mailad/le/fullchain.pem /etc/ssl/certs/mail.crt
+    ln -s /etc/mailad/le/fullchain.pem /etc/ssl/certs/cacert.pem
+    ln -s /etc/mailad/le/privkey.key /etc/ssl/private/mail.key
 
-# Generate self signed root CA cert
-openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj \
-    "/C=$SSLPAIS/ST=$SSLESTADO/L=$SSLCIUDAD/O=$SSLEMPRESA/OU=$SSLUEB/CN=$SSLHOSTNAME/emailAddress=$ADMINMAIL"
+else
+    echo "Generating a Self Signed Certificate for this node"
 
-# Generate server cert to be signed
-openssl req -nodes -newkey rsa:2048 -keyout mail.key -out mail.csr -subj \
-    "/C=$SSLPAIS/ST=$SSLESTADO/L=$SSLCIUDAD/O=$SSLEMPRESA/OU=$SSLUEB/CN=$SSLHOSTNAME/emailAddress=$ADMINMAIL"
+    # moving to a temp dir to work
+    TMP=`mktemp -d`
+    cd "$TMP"
 
-# Sign the server cert (valid by 3 years)
-openssl x509 -req -days 1095 -in mail.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out mail.crt
+    # Generate self signed root CA cert
+    openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj \
+        "/C=$SSLPAIS/ST=$SSLESTADO/L=$SSLCIUDAD/O=$SSLEMPRESA/OU=$SSLUEB/CN=$SSLHOSTNAME/emailAddress=$ADMINMAIL"
 
-# create the server pem file
-cat ca.key ca.crt > cacert.pem
+    # Generate server cert to be signed
+    openssl req -nodes -newkey rsa:2048 -keyout mail.key -out mail.csr -subj \
+        "/C=$SSLPAIS/ST=$SSLESTADO/L=$SSLCIUDAD/O=$SSLEMPRESA/OU=$SSLUEB/CN=$SSLHOSTNAME/emailAddress=$ADMINMAIL"
 
-#move it to the final places & fix perms
-mv -f mail.key /etc/ssl/private/
-mv -f mail.crt /etc/ssl/certs/
-mv -f cacert.pem /etc/ssl/certs/
-chmod 0600 /etc/ssl/private/mail.key
-chmod 0600 /etc/ssl/certs/mail.crt
-chmod 0600 /etc/ssl/certs/cacert.pem
+    # Sign the server cert (valid by 3 years)
+    openssl x509 -req -days 1095 -in mail.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out mail.crt
 
-# clan the workspace for dhparam generation
-rm *
+    # create the server pem file
+    cat ca.key ca.crt > cacert.pem
+
+    #move it to the final places & fix perms
+    mv -f mail.key /etc/ssl/private/
+    mv -f mail.crt /etc/ssl/certs/
+    mv -f cacert.pem /etc/ssl/certs/
+    chmod 0600 /etc/ssl/private/mail.key
+    chmod 0600 /etc/ssl/certs/mail.crt
+    chmod 0600 /etc/ssl/certs/cacert.pem
+
+    # clan the workspace for dhparam generation
+    rm *
+fi
 
 ## dhparms generation
 echo "Generation of SAFE dhparam, this will take a time, be patient..."
@@ -64,8 +77,3 @@ chmod 0644 /etc/ssl/dh/RSA2048.pem
 # clean the house
 cd ~
 rm -rdf "$TMP"
-
-echo "All certs generated and in place..."
-echo "Success!"
-
-exit 0
