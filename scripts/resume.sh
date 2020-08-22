f3a06aadf062686605093287d7abfdb13e10280f
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2020-08-21 23:27:01 -0400

    Refs #37, add the script that builds the symmary

---
+++ scripts/resume.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script is part of MailAD, see https://github.com/stdevPavelmc/mailad/
+# Copyright 2020 Pavel Milanes Costa <pavelmc@gmail.com>
+# LICENCE: GPL 3.0 and later  
+#
+# Goals:
+#   - Create a resume of yesterday mail services
+#   - Send it to the mail admin
+#   - Must be preogrammed to run about 7:30am every morning
+
+# import config
+source /etc/mailad/mailad.conf
+
+# loading vars
+PFSL=`which pflogsumm`
+OPTS="-d yesterday -e -i --iso-date-time --problems-first"
+FILE="/var/log/mail.log /var/log/mail.log.1"
+TMP=`tempfile`
+
+# check for soft
+if [ "$PFSL" == "" ] ; then
+    # no soft installed, warining
+    echo "MailAD: Can't make the mail traffic summary because pflogsumm sotware is missing!"
+    exit 1
+fi
+
+# ejecutando
+$PFSL $OPTS $FILE > $TMP
+
+# enviar el correo
+cat $TMP | mail -s "MailAD: Yesterday's mail traffic Summary" ${ADMINMAIL}
+rm $TMP
