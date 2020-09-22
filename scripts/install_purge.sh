f34fe3b973e2a2067c7341ca1e7d4a3aa2c7fc0b
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2020-09-22 13:42:01 -0400

    Refs #68, detecting the OS on the install purge step, to purge the correct pkgs and all at once so apt can do it's best

--- scripts/install_purge.sh
+++ scripts/install_purge.sh
@@ -11,10 +11,47 @@
 source /etc/mailad/mailad.conf
 source common.conf
 
-# list of pkgs to install came from common.conf
+PKGS=""
+
+# list of pkgs to install came from common.conf, pick the list to uninstall
+if [ -f /etc/os-release ] ; then
+    # import the file
+    source /etc/os-release
+
+    ##### Distros check
+
+    # Ubuntu bionic/focal (18.04/20.04) LTS
+    if [ "$VERSION_CODENAME" == "bionic" -o "$VERSION_CODENAME" == "focal" ] ; then
+        # notice
+        echo "===> We are working with $PRETTY_NAME"
+
+        # load the correct pkgs to be installed
+        PKGS=${UBUNTU_PKGS}
+    fi
+
+    # Debian Buster (10.x)
+    if [ "$VERSION_CODENAME" == "buster"  ] ; then
+        # notice
+        echo "===> We are working with $PRETTY_NAME"
+
+        # load the correct pkgs to be installed
+        PKGS=${DEBIAN_PKGS}
+    fi
+else
+    # not known
+    echo "==========================================================================="
+    echo "ERROR: This linux box has a not known distro, if you feel this is wrong"
+    echo "       please visit ttps://github.com/stdevPavelmc/mailad/ and raise an"
+    echo "       issue about this."
+    echo "==========================================================================="
+    echo "       The uninstall process will stop now"
+    echo "==========================================================================="
+fi
+
+# sort pkgs
 
 # remove all pkgs letting apt build the tree
-env DEBIAN_FRONTEND=noninteractive apt-get purge $PKGCOMMON -y
+env DEBIAN_FRONTEND=noninteractive apt-get purge $PKGCOMMON $PKGS -y
 
 # autoremove some of the pkgs left over
 env DEBIAN_FRONTEND=noninteractive apt autoremove -y
