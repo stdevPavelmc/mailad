befef7d0f4a74855fcd5acb196305d363be7f859
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2025-04-08 22:04:46 -0400

    Improve the feedback message generation and contents

--- scripts/feedback.sh
+++ scripts/feedback.sh
@@ -17,13 +17,18 @@ source /etc/mailad/mailad.conf
 source common.conf
 source /etc/os-release
 
+# get installs stats
+FIRST_INSTALL=$(grep "^FIRST_INSTALL=" $INSTFILE | cut -d'=' -f2)
+INSTALLS=$(grep "^INSTALLS=" $INSTFILE | cut -d'=' -f2)
+
 # Infos to send
-TO="pavelmc@gmail.com,$ADMINMAIL"
-DATE=`date +"%Y/%m/%d %X %Z"`
-VERSION=`cat CHANGELOG.md  | grep "##" | head -n 1 | cut -d ' ' -f 2`
+TO="$AUTHOR,$ADMINMAIL"
+DATE=$(date +"%Y/%m/%d %X %Z")
+VERSION=$(cat CHANGELOG.md  | grep "##" | head -n 1 | cut -d ' ' -f 2)
 
 # build the email
-BODY=`mktemp`
+GIT_TREE=$(echo $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse --short HEAD))
+BODY=$(mktemp)
 echo "Hi!" > $BODY
 echo " " >> $BODY
 echo "This is an instance of MailAD giving you feedback!" >> $BODY
@@ -32,12 +37,13 @@ echo "Date: $DATE" >> $BODY
 echo "Domain: $DOMAIN" >> $BODY
 echo "Mailserver: $HOSTNAME" >> $BODY
 echo "Mail_Admin: $ADMINMAIL" >> $BODY
-echo "OS: $PRETTY_NAME=" >> $BODY
+echo "OS: $PRETTY_NAME" >> $BODY
 echo "MailAD version: $VERSION" >> $BODY
+echo "Git tree anchor: $GIT_TREE" >> $BODY
 echo "First install: $FIRST_INSTALL"  >> $BODY
 echo "Install count: $INSTALLS"  >> $BODY
 echo " " >> $BODY
 echo "EOT, thanks!" >> $BODY
 
 # sent mail
-send_email "MailAD stats service." "$ADMINMAIL" "$BODY"
+send_email "MailAD stats service." "$TO" "$BODY"
