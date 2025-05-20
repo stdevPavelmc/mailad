31d29d9fbd4b702d1ec499da6f2952f738b4858c
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2025-05-20 18:09:16 -0400

    Add missing file

---
+++ tests/test-from-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script is part of MailAD, see https://github.com/stdevPavelmc/mailad/
+# Copyright 2025 Pavel Milanes Costa <pavelmc@gmail.com>
+# LICENCE: GPL 3.0 and later  
+#
+# Goal:
+#   - run make test $IP from inside a virgin docker container
+#   - get a IP to test the server from the first command.
+
+# update the system
+apt-get update
+
+# install make
+apt-get install -y make
+
+# move to working dir
+cd /home/mailad/
+
+# install base conf
+make conf
+
+# Ran tests
+make test "$1"
